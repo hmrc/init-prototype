a5fd658b3755c47183f45afe0d4fa2360698895b
Matthew Mascord <27346863+matthewmascord@users.noreply.github.com>
2020-08-04 15:52:27 +0100

    Add missing explicit main class

--- project/AssemblySettings.scala
+++ project/AssemblySettings.scala
@@ -4,18 +4,19 @@ import sbtassembly.AssemblyKeys._
 import sbtassembly._
 
 object AssemblySettings {
-  def apply()= Seq(
+  def apply() = Seq(
     assemblyJarName in assembly := "init-prototype.jar",
+    mainClass in assembly := Some("uk.gov.hmrc.initprototype.Main"),
     assemblyMergeStrategy in assembly := {
-      case "module-info.class" => MergeStrategy.discard
-      case PathList("org", "apache", "commons", "logging", xs@_*) => MergeStrategy.first
-      case PathList("play", "core", "server", xs@_*) => MergeStrategy.first
+      case "module-info.class"                                      => MergeStrategy.discard
+      case PathList("org", "apache", "commons", "logging", xs @ _*) => MergeStrategy.first
+      case PathList("play", "core", "server", xs @ _*)              => MergeStrategy.first
       case x =>
         val oldStrategy = (assemblyMergeStrategy in assembly).value
         oldStrategy(x)
     },
-    artifact in(Compile, assembly) := {
-      val art = (artifact in(Compile, assembly)).value
+    artifact in (Compile, assembly) := {
+      val art = (artifact in (Compile, assembly)).value
       art.withClassifier(Some("assembly"))
     }
   )
