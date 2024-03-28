3b31cbf0bc12d4d9febe36ca208390b4fe47b65d
TimothyFothergill <67912934+TimothyFothergill@users.noreply.github.com>
2024-03-28 15:00:14 +0000

    PLATUI-2906 remove further unused dependencies

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -7,8 +7,6 @@ object LibDependencies {
     "ch.qos.logback"              % "logback-classic" % "1.2.3",
     "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.5",
     "commons-io"                  % "commons-io"      % "2.7",
-    "com.typesafe.play"          %% "play-json"       % "2.9.0",
-    "com.typesafe"                % "config"          % "1.4.0",
     "com.lihaoyi"                %% "os-lib"          % "0.7.8"
   )
 }
